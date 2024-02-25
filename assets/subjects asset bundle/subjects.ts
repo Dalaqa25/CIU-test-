301ce1a9fba3238cacd5129db49dcb5ded2ee43a
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-02-25 11:50:26 +0400

    first commit

---
+++ assets/subjects asset bundle/subjects.ts
@@ -0,0 +1,17 @@
+const subjects = 
+[
+    {
+        id:1,
+        name:'Linaer Algebra',
+        image:
+        'https://upload.wikimedia.org/wikipedia/commons/c/c3/Deus_mathematics.png',
+    },
+    {
+        id:2,
+        name:'Object Orianted Programing',
+        image:
+        'https://cdn-icons-png.flaticon.com/512/4248/4248245.png',
+    },
+]
+
+export default subjects;
